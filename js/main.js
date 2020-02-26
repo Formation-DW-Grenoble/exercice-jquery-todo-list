223545fda29bd7339d9b97a104fce21cb7212149
Romain Tello <romain.tello@gmail.com>
2020-02-26 11:45:05 +0100

    Add name change form to to-do items

--- js/main.js
+++ js/main.js
@@ -2,15 +2,39 @@ function createNewTodo(text) {
   // Crée un nouvel élément à partir de son code HTML
   var newElement = $(
     '<li class="todo-item list-group-item list-group-item-action d-flex justify-content-between align-items-center">' +
+    '<div class="todo-name">' +
     text +
+    '</div>' +
     '</li>'
   );
   // Crée un nouveau bouton de suppression
   var deleteButton = createDeleteButton();
   // Rajoute le bouton de suppression dans les enfants du nouvel élément
   newElement.append(deleteButton);
+
+  var nameChangeForm = createNameChangeForm();
+
+  newElement.prepend(nameChangeForm);
   // Renvoie le nouvel élément
   return newElement;
+}
+
+function createNameChangeForm() {
+  // Crée un nouveau formulaire avec un champ texte et un bouton de validation
+  var newForm = $(
+    '<form class="d-flex"><input class="todo-name" type="text"/><button type="submit" class="btn btn-success btn-sm"><i class="fas fa-check"></i></button></form>'
+  );
+  // Associe une action au fait de valider le formulaire
+  newForm.on( 'submit', function(event) {
+    // Empêche le formulaire de s'envoyer normalement
+    event.preventDefault();
+    // Récupère le texte écrit par l'utilisateur dans le champ de changement de nom
+    var text = $(event.target).children('input.todo-name').val();
+    // Remplace le nom de la tâche à faire par le texte récupéré précédemment
+    $(event.target).nextAll('.todo-name').html(text);
+  });
+  // Renvoie le nouveau formulaire
+  return newForm;
 }
 
 function createDeleteButton() {

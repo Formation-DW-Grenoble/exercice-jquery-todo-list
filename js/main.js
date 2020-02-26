db074ef1d6c7d473fa9f93d0965c6dd9aa2a38fe
Romain Tello <romain.tello@gmail.com>
2020-02-26 12:18:49 +0100

    Add name change form toggle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 function createNewTodo(text) {
   // Crée un nouvel élément à partir de son code HTML
   var newElement = $(
-    '<li class="todo-item list-group-item list-group-item-action d-flex justify-content-between align-items-center">' +
-    '<div class="todo-name">' +
+    '<li class="todo-item list-group-item list-group-item-action justify-content-between align-items-center">' +
+    '<span class="todo-name">' +
     text +
-    '</div>' +
+    '</span>' +
     '</li>'
   );
+
+  var nameChangeButton = createNameChangeButton();
+
+  newElement.append(nameChangeButton);
   // Crée un nouveau bouton de suppression
   var deleteButton = createDeleteButton();
   // Rajoute le bouton de suppression dans les enfants du nouvel élément
@@ -22,16 +26,20 @@ function createNewTodo(text) {
 function createNameChangeForm() {
   // Crée un nouveau formulaire avec un champ texte et un bouton de validation
   var newForm = $(
-    '<form class="d-flex"><input class="todo-name" type="text"/><button type="submit" class="btn btn-success btn-sm"><i class="fas fa-check"></i></button></form>'
+    '<form class="name-change-form d-none"><div class="d-flex"><input class="todo-name" type="text"/><button type="submit" class="btn btn-success btn-sm"><i class="fas fa-check"></i></button></div></form>'
   );
   // Associe une action au fait de valider le formulaire
   newForm.on( 'submit', function(event) {
     // Empêche le formulaire de s'envoyer normalement
     event.preventDefault();
     // Récupère le texte écrit par l'utilisateur dans le champ de changement de nom
-    var text = $(event.target).children('input.todo-name').val();
+    var text = $(event.target).find('input.todo-name').val();
     // Remplace le nom de la tâche à faire par le texte récupéré précédemment
-    $(event.target).nextAll('.todo-name').html(text);
+    $(event.target).nextAll('span.todo-name').html(text);
+    // Rajouter au formulaire la classe qui le rend invisible
+    $(event.target).parents('li.todo-item').children('.name-change-form').addClass('d-none');
+    // Enlever du nom la classe qui le rend invisible
+    $(event.target).parents('li.todo-item').children('span.todo-name').removeClass('d-none');    
   });
   // Renvoie le nouveau formulaire
   return newForm;
@@ -39,7 +47,7 @@ function createNameChangeForm() {
 
 function createDeleteButton() {
   // Crée un nouveau bouton de suppression
-  var deleteButton = $('<button class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i></button>');
+  var deleteButton = $('<button class="btn btn-sm btn-danger float-right"><i class="fas fa-trash-alt"></i></button>');
   // Associe le bouton de suppression à l'action qui permet de supprimer le nouvel élément
   deleteButton.on( 'click', function(event) {
     // Cherche parmi les parents de l'élément qui a déclenché la fonction, celui qui a la classe "todo-item"
@@ -48,6 +56,17 @@ function createDeleteButton() {
   });
   // Renvoie le nouveau bouton
   return deleteButton;
+}
+
+function createNameChangeButton() {
+  var nameChangeButton = $('<button class="btn btn-primary btn-sm float-right"><i class="fas fa-pen"></i></button>');
+  nameChangeButton.on( 'click', function(event) {
+    // Enlever du formulaire la classe qui le rend invisible
+    $(event.target).parents('li.todo-item').children('.name-change-form').removeClass('d-none');
+    // Rajouter au nom la classe qui le rend invisible
+    $(event.target).parents('li.todo-item').children('span.todo-name').addClass('d-none');
+  })
+  return nameChangeButton;
 }
 
 // Lorsque le formulaire est envoyé

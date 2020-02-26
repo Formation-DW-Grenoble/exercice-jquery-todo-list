00cdff851f4871480910c7cf5c191fd5ade59123
Romain Tello <romain.tello@gmail.com>
2020-02-26 10:09:07 +0100

    Add todo delete button

--- js/main.js
+++ js/main.js
@@ -4,10 +4,20 @@ $('#add-todo').on( 'submit', function(event) {
   event.preventDefault();
   // Crée un nouvel élément à partir de son code HTML
   var newElement = $(
-    '<li class="list-group-item list-group-item-action">' +
+    '<li class="todo-item list-group-item list-group-item-action d-flex justify-content-between align-items-center">' +
     $('#caption').val() +
     '</li>'
   );
+  // Crée un nouveau bouton de suppression
+  var deleteButton = $('<button class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i></button>');
+  // Associe le bouton de suppression à l'action qui permet de supprimer le nouvel élément
+  deleteButton.on( 'click', function(event) {
+    // Cherche parmi les parents de l'élément qui a déclenché la fonction, celui qui a la classe "todo-item"
+    // et le supprime de la page
+    $(event.target).parents('.todo-item').remove();
+  });
+  // Rajoute le bouton de suppression dans les enfants du nouvel élément
+  newElement.append(deleteButton);
   // Ajoute cet élément dans la liste
   $('#list').append(newElement);
   // Efface le contenu actuel du champ texte
